8ae9da995de48aaf31647ed8d278641e76e6fc7a
Chen Barsano <chenbarsano@gmail.com>
2022-06-15 22:14:21 +0300

    more dark mode :)

--- app/src/main/java/com/example/circlechat/SettingsActivity.java
+++ app/src/main/java/com/example/circlechat/SettingsActivity.java
@@ -4,17 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
+
+import com.example.circlechat.databinding.ActivityChatBinding;
+import com.example.circlechat.databinding.ActivitySettingsBinding;
 
 public class SettingsActivity extends AppCompatActivity {
     SwitchCompat switchCompat;
     SharedPreferences sharedPreferences = null;
+    ActivitySettingsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, ChatListActivity.class);
+                startActivity(intent);
+            }
+        });
 
         switchCompat = findViewById(R.id.switchTheme);
         sharedPreferences = getSharedPreferences("night",0);
