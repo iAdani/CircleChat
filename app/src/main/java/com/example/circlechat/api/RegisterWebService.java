8f45140d596fc5f01bdef01d288f1a3d04fd376b
Guy Adani <guy.adani@live.biu.ac.il>
2022-06-16 14:17:30 +0300

    Register now works with the server

--- app/src/main/java/com/example/circlechat/api/RegisterWebService.java
+++ app/src/main/java/com/example/circlechat/api/RegisterWebService.java
@@ -6,8 +6,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.example.circlechat.ChatListActivity;
-import com.example.circlechat.LoginActivity;
 import com.example.circlechat.R;
+import com.example.circlechat.RegisterActivity;
 import com.example.circlechat.entities.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -18,42 +18,42 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class LoginWebService {
-    private final LoginWebServiceAPI loginWebServiceAPI;
-    private final LoginActivity loginActivity; // The context
+public class RegisterWebService {
+    private final RegisterWebServiceAPI registerWebServiceAPI;
+    private final RegisterActivity registerActivity;
 
-    public LoginWebService(LoginActivity loginActivity) {
-        this.loginActivity = loginActivity;
+    public RegisterWebService(RegisterActivity registerActivity) {
+        this.registerActivity = registerActivity;
 
         // Building the retrofit
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
         final Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(this.loginActivity.getString(R.string.BaseUrl))
+                .baseUrl(this.registerActivity.getString(R.string.BaseUrl))
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
-        loginWebServiceAPI = retrofit.create(LoginWebServiceAPI.class);
+        registerWebServiceAPI = retrofit.create(RegisterWebServiceAPI.class);
     }
 
-    public void login(String username, String password) {
-        // Creating a login call to the server
-        Call<String> call = loginWebServiceAPI.Login(new User(username, password));
-        loginActivity.runOnUiThread(() -> call.enqueue(new Callback<String>() {
+    public void register(String username, String password) {
+        // Creating a register call to the server
+        Call<String> call = registerWebServiceAPI.Register(new User(username, password));
+        registerActivity.runOnUiThread(() -> call.enqueue(new Callback<String>() {
             @Override
             public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
                 if(response.isSuccessful()) {
                     // If 200OK move to chats
-                    Intent intent = new Intent(loginActivity, ChatListActivity.class);
-                    loginActivity.startActivity(intent);
+                    Intent intent = new Intent(registerActivity, ChatListActivity.class);
+                    registerActivity.startActivity(intent);
                 } else {
-                    Toast.makeText(loginActivity, "Invalid username or password.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(registerActivity, "Sorry, this username already exists.", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
-                Toast.makeText(loginActivity, "Cannot find the server.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(registerActivity, "Cannot find the server.", Toast.LENGTH_SHORT).show();
             }
         }));
     }
---
+++ app/src/main/java/com/example/circlechat/api/RegisterWebServiceAPI.java
@@ -0,0 +1,12 @@
+package com.example.circlechat.api;
+
+import com.example.circlechat.entities.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RegisterWebServiceAPI {
+    @POST("register")
+    Call<String> Register(@Body User user);
+}
